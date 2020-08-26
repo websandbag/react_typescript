b42bebab0b26aa2f104c2545c6ac2a84b0d20eeb
中橋博文 <nakahashi_h@websandbag.com>
2020-08-27 03:02:57 +0900

    stylus設定

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Hello from "./components/Hello";
 
+import '../stylus/index.styl';
+
 ReactDOM.render(<Hello content="hello world"/>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: './src/scripts/index.tsx',
@@ -16,17 +18,45 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_module/,
+                exclude: /node_modules/,
             },
             {
                 test: /\.pug$/,
                 use: 'pug-loader'
+            },
+            {
+                test: /\.styl$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    grid: true,
+                                    flexbox: true
+                                })
+                            ]
+                        }
+                    },
+                    'stylus-loader'
+                ]
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/pug/index.pug"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
         })
     ]
 }
