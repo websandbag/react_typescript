c5ad8dbc299d589ce3c4d963aa62d26440111afa
中橋博文 <nakahashi_h@websandbag.com>
2020-08-29 04:18:02 +0900

    テストコード作成

--- src/scripts/counter/__tests__/ActionDispatcher.spec.ts
+++ src/scripts/counter/__tests__/ActionDispatcher.spec.ts
@@ -6,11 +6,11 @@ import { ActionDispatcher } from '../Container';
  */
 describe('ActionDispatcher', () => {
     it('increment', () => {
-        const spy: any = {dispatch: null}
-        spyOn(spy, 'dispatch');
+        const spy: any = { dispatch: null }
+        spyOn(spy, 'dispatch')
         const actions = new ActionDispatcher(spy.dispatch)
         actions.increment(100)
-        expect(spy.dispatch.calls.count().toEqual(1));
-        expect(spy.dispatch.calls.argsFor(0)[0]).toEqual(incrementAmount(10));
+        expect(spy.dispatch.calls.count()).toEqual(1)
+        expect(spy.dispatch.calls.argsFor(0)[0]).toEqual(incrementAmount(100))
     })
 })
