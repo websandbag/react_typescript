82dce8abd8e2b4c6e059381c815b6a9cb4706f0c
中橋博文 <nakahashi_h@websandbag.com>
2020-08-29 20:32:13 +0900

    Props設定

--- src/scripts/counter/Counter.tsx
+++ src/scripts/counter/Counter.tsx
@@ -1,20 +1,18 @@
-import * as React from "react";
-import { CounterState } from "./module";
-import { ActionDispatcher } from "./Container";
+import * as React from 'react'
+import { CounterState } from './module'
+import { ActionDispatcher } from './Container'
 
 interface Props {
-    value: CounterState,
+    value: CounterState
     actions: ActionDispatcher
 }
 
-export class Counter extends React.Component<Props, {}> {
-    render() {
-        return (
-            <div>
-                <p>score: {this.props.value.num}</p>
-                <button onClick={() => this.props.actions.increment(3)}>Increment 3</button>
-                <button onClick={() => this.props.actions.decrement(2)}>Decrement 2</button>
-            </div>
-        );
-    }
+export function Counter(props: Props) {
+    return (
+        <div>
+            <p>{`score: ${props.value.num}`}</p>
+            <button onClick={() => props.actions.increment(3)}>Increment 3</button>
+            <button onClick={() => props.actions.decrement(2)}>Decrement 2</button>
+        </div>
+    )
 }
