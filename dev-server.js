9f45e9cdaad72bcb60a19f2e5b85b27e8e94ce8d
中橋博文 <nakahashi_h@websandbag.com>
2020-09-22 16:02:58 +0900

    初期ページ用意

--- dev-server.js
+++ dev-server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('./dist'));
 
 app.get('/api/count', (req, res) => {
     res.contentType('application/json');
